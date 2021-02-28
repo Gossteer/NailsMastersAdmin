b08308b76f4ff448644ea9d821c414cef9491819
Gossteer <GossteerPlus@gmai.com>
2021-02-28 22:10:24 +0300

    userpassword

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -14,23 +15,27 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'id' => 1,
-            'name' => 'Admin Admin',
-            'email' => 'admin@admin.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('123456789'),
-            'created_at' => now(),
-            'updated_at' => now()
+        User::find(1)->update([
+            'password' =>
+            bcrypt("OniOna232NaNa_GossteerOn")
         ]);
-        DB::table('users')->insert([
-            'id' => 2,
-            'name' => 'Admin Admin',
-            'email' => 'user@admin',
-            'email_verified_at' => now(),
-            'password' => Hash::make('123'),
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+        // DB::table('users')->insert([
+        //     'id' => 1,
+        //     'name' => 'Admin Admin',
+        //     'email' => 'admin@admin.com',
+        //     'email_verified_at' => now(),
+        //     'password' => Hash::make('123456789'),
+        //     'created_at' => now(),
+        //     'updated_at' => now()
+        // ]);
+        // DB::table('users')->insert([
+        //     'id' => 2,
+        //     'name' => 'Admin Admin',
+        //     'email' => 'user@admin',
+        //     'email_verified_at' => now(),
+        //     'password' => Hash::make('123'),
+        //     'created_at' => now(),
+        //     'updated_at' => now()
+        // ]);
     }
 }
